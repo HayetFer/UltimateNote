fc3ddbcff1c6ef743c2b7794558f4d0aa740d981
HayetFer <ferahihayet@gmail.com>
2023-04-23 01:23:16 +0200

    added functionnalities| google drive api , to do : add a data visualisation and synch all data with google drive

---
+++ WinFormsApp1/IDDrive.cs
@@ -0,0 +1,55 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class IDDrive
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string IdDB { get; set; }
+
+
+        public IDDrive() { }
+        public IDDrive(string t)
+        {
+            IdDB=t;
+        }
+        public string GetFirstElement()
+        {
+            using (var connection = new SQLiteConnection("Data Source=yourdatabase.db"))
+            {
+                using (var command = new SQLiteCommand("SELECT * FROM IDDrive LIMIT 1", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return reader.GetString(reader.GetOrdinal("IdDB"));
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void UpdateById(int id, string newIdDB)
+        {
+            using (var db = new SQLiteConnection(@"D:\\UltimateNote\\UltimateNote\mydatabase.db3"))
+            {
+                var recordToUpdate = db.Table<IDDrive>().FirstOrDefault(x => x.ID == id);
+                if (recordToUpdate != null)
+                {
+                    recordToUpdate.IdDB = newIdDB;
+                    db.Update(recordToUpdate);
+                }
+            }
+        }
+    }
+}
+
